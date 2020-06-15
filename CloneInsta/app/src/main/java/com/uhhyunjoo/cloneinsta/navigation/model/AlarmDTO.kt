8ad0ed15e8afe38d162393a996812f43054392a6
uhhyunjoo <alro923@naver.com>
2020-06-15 22:42:38 +0900

    alarm list page

--- CloneInsta/app/src/main/java/com/uhhyunjoo/cloneinsta/navigation/model/AlarmDTO.kt
+++ CloneInsta/app/src/main/java/com/uhhyunjoo/cloneinsta/navigation/model/AlarmDTO.kt
@@ -5,6 +5,10 @@ data class AlarmDTO(
     var userId: String? = null,
     var uid: String? = null,
     var kind: Int? = null,
+    // kind == alarm 을 구분해주는 flag 값
+    // 0 : like alarm
+    // 1 : comment alarm
+    // 2 : follow alarm
     var message: String? = null,
     var timestamp: Long? = null
 )
